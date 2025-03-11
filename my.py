30c736680cdaaa48403c33b925e31ba23197e27e
Матвiй <matvijgrinisak@gmail.com>
2025-03-10 14:53:32 -1200

    add

--- my.py
+++ my.py
@@ -1,4 +1,23 @@
-print("hello")
-a = 5
-print(a+4)
-print(a+5)
+num1 = float(input("num1="))
+num2 = float(input("num2="))
+operation = input("operation=")
+
+def calc(num1,num2,operation):
+    if operation == "+":
+        return num1+num2
+
+    elif operation == "-":
+        return num1 - num2
+
+    elif operation == "*":
+        return num1 * num2
+
+    elif operation == "/":
+        return num1 / num2
+
+print(calc(num1, num2,operation))
+
+
+
+
+
