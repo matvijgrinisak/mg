048f39dc66e4154e62319f00ba5a3733577eced6
Матвiй <matvijgrinisak@gmail.com>
2025-03-20 15:32:52 -1200

    add

--- my.py
+++ my.py
@@ -1,23 +1,25 @@
-num1 = float(input("num1="))
-num2 = float(input("num2="))
-operation = input("operation=")
 
-def calc(num1,num2,operation):
-    if operation == "+":
-        return num1+num2
 
-    elif operation == "-":
-        return num1 - num2
+while True:
+    num1 = float(input("num1= "))
+    num2 = float(input("num2= "))
+    operation = input("operation= ")
 
-    elif operation == "*":
-        return num1 * num2
-
-    elif operation == "/":
-        return num1 / num2
+    def calc(num1, num2, operation):
+        if operation == "+":
+           return num1 + num2
+        elif operation == "-":
+           return num1 - num2
+        elif operation == "*":
+           return num1 * num2
+        elif operation == "/":
+           return num1 / num2
+        else:
+            print("Error")
 
 print(calc(num1, num2,operation))
 
-
-
+if operation == "0":
+    print("Error")
 
 
