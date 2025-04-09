cd9619e9dd1d0dbe8ad7b6325d65d1390de6e081
Матвiй <matvijgrinisak@gmail.com>
2025-04-08 16:41:48 -1200

    add

--- my.py
+++ my.py
@@ -1,37 +1,47 @@
-class Product:
-    def __init__(self, name = "None", price = 0):
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    def __init__(self, name, age):
         self.name = name
-        self.price = price
-    def info(self):
-        print(f"name - {self.name}\ price - {self.price} грн")
-
-class Customer:
-    def __init__(self, name = "Ігор", cart = []):
-        self.name = name
-        self.cart = cart
-    def add_to_cart(self, obj: Product):
-        self.cart.append(obj)
-    def show_cart(self):
-        total_price = 0
-        print(f"покупець - {self.name}")
-        for i in self.cart:
-            print("додано до кошика:", i.name, i.price)
-            total_price += i.price
-        print("загальна вартість", total_price)
-d = Product("fsfqev", 200)
-d1 = Product("fewvew", 230)
-d2 = Product("fembjw", 180)
-d3 = Product("kvjiis", 150)
-d.info()
-d1.info()
-d2.info()
-d3.info()
-r = Customer()
-r.add_to_cart(d)
-r.add_to_cart(d1)
-r.add_to_cart(d2)
-r.add_to_cart(d3)
-r.show_cart()
+        self.age = age
 
 
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+
+    def get_info(self):
+        return f"Ім'я: {self.name}, Вік: {self.age}"
+
+
+
+class Dog(Animal):
+    def make_sound(self):
+        return "Гав-гав!"
+
+
+
+class Cat(Animal):
+    def make_sound(self):
+        return "Няв-няв!"
+
+
+
+class Parrot(Animal):
+    def make_sound(self):
+        return "Привіт!"
+
+
+
+dog = Dog("Шерлок", 5)
+cat = Cat("Мурка", 3)
+parrot = Parrot("Кеша", 2)
+
+
+animals = [dog, cat, parrot]
+for animal in animals:
+    print(animal.get_info())
+    print(animal.make_sound())
+    print()
 
